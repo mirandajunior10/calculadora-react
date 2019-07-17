ae2329c104748bbcbae148c6b5581c8c37bd14df
mirandajunior10 <jorlanjr@gmail.com>
2019-07-17 15:22:41 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './main/Calculator';
+
+const elemento = document.getElementById('root');
+
+ReactDOM.render(
+    <div>
+        <h1>Calculadora</h1>
+        <Calculator />
+
+    </div>
+
+, elemento);
+
+
